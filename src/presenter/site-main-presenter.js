0cdbc47d04b7429fe5283d23dcd5c97bfd44ac76
ArinaKo <arinakovalenco7@gmail.com>
2023-05-01 20:12:19 +0300

    Добавляет компоненты событий

--- src/presenter/site-main-presenter.js
+++ src/presenter/site-main-presenter.js
@@ -1,9 +1,12 @@
 import SortingView from '../view/sorting-view.js';
 import SortingItemView from '../view/sorting-item-view.js';
+import EventsListView from '../view/events-list-view.js';
+import EventView from '../view/event-view.js';
 import { render } from '../render.js';
 
 export default class SiteMainPresenter {
   sortingComponent = new SortingView();
+  eventsListComponent = new EventsListView();
 
   constructor({ container }) {
     this.container = container;
@@ -12,6 +15,10 @@ export default class SiteMainPresenter {
   init() {
     render(this.sortingComponent, this.container);
     render(new SortingItemView(), this.sortingComponent.getElement());
+    render(this.eventsListComponent, this.container);
+    for (let i = 1; i <= 3; i++) {
+      render(new EventView(), this.eventsListComponent.getElement());
+    }
   }
 }
 
