85074b48945a4f4faba2f81e3670a4fd64372373
ArinaKo <arinakovalenco7@gmail.com>
2023-05-02 17:43:53 +0300

    Добавляет компоненты секций формы

--- src/presenter/form-presenter.js
+++ src/presenter/form-presenter.js
@@ -1,10 +1,13 @@
 import FormView from '../view/form-view.js';
 import FormMainView from '../view/form-main-view.js';
-import { render/*, RenderPosition*/ } from '../render.js';
+import FormDetailsView from '../view/form-details-view.js';
+import FormSectionView from '../view/form-section-view.js';
+import { render } from '../render.js';
 
 export default class FormPresenter {
   formComponent = new FormView();
   formMainComponent = new FormMainView();
+  formDetailsComponent = new FormDetailsView();
 
   constructor({ container }) {
     this.container = container;
@@ -13,5 +16,8 @@ export default class FormPresenter {
   init() {
     render(this.formComponent, this.container);
     render(this.formMainComponent, this.formComponent.getElement());
+    render(this.formDetailsComponent, this.formComponent.getElement());
+    render(new FormSectionView(), this.formDetailsComponent.getElement());
+    render(new FormSectionView(), this.formDetailsComponent.getElement());
   }
 }
