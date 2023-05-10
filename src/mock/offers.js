0cb2369d5b4a7bb8c7eb91de69f70a1c99fc7d3b
ArinaKo <arinakovalenco7@gmail.com>
2023-05-10 23:34:36 +0300

    Изменяет структуру данных дополнительных опций

--- src/mock/offers.js
+++ src/mock/offers.js
@@ -1,6 +1,8 @@
 import { getRandomArrayElement } from '../utils.js';
 import { EVENTS_TYPES } from '../const.js';
 
+const OFFERS_NUMBER = 5;
+
 const OFFERS_TITLES = [
   'Upgrade',
   'Downgrade',
@@ -13,40 +15,21 @@ const OFFERS_TITLES = [
 const OFFERS_PRICES = [100, 13, 75, 40, 5, 66];
 
 function createMockOffers() {
-  return [
-    {
-      id: 1,
+  const offers = new Map();
+
+  for (let i = 1; i <= OFFERS_NUMBER; i ++) {
+    offers.set(i, {
       title: getRandomArrayElement(OFFERS_TITLES),
       price: getRandomArrayElement(OFFERS_PRICES),
-    },
-    {
-      id: 2,
-      title: getRandomArrayElement(OFFERS_TITLES),
-      price: getRandomArrayElement(OFFERS_PRICES),
-    },
-    {
-      id: 3,
-      title: getRandomArrayElement(OFFERS_TITLES),
-      price: getRandomArrayElement(OFFERS_PRICES),
-    },
-    {
-      id: 4,
-      title: getRandomArrayElement(OFFERS_TITLES),
-      price: getRandomArrayElement(OFFERS_PRICES),
-    },
-    {
-      id: 5,
-      title: getRandomArrayElement(OFFERS_TITLES),
-      price: getRandomArrayElement(OFFERS_PRICES),
-    },
-  ];
+    });
+  }
+  return offers;
 }
 
 function createMockOffersByTypes() {
-  return EVENTS_TYPES.map((title) => ({
-    type: title,
-    offers: createMockOffers(),
-  }));
+  const mockOffers = new Map();
+  EVENTS_TYPES.forEach((type) => mockOffers.set(type, createMockOffers()));
+  return mockOffers;
 }
 
 export { createMockOffersByTypes };
