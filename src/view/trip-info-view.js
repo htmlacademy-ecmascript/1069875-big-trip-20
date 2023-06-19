a2232b7b6ac009e3db2616b7e6893c7c47644841
ArinaKo <arinakovalenco7@gmail.com>
2023-06-19 17:29:27 +0300

    Дополняет логику отрисовки информации о путешествии

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -1,21 +1,51 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import dayjs from 'dayjs';
+import { DateFormats } from '../const.js';
 
-function createTripInfoTemplate() {
+function createTitle(destinations) {
+  return `${destinations.start}${destinations.middle ? ` &mdash; ${destinations.middle}` : ''}${destinations.end ? ` &mdash; ${destinations.end}` : ''}`;
+}
+
+function createTripInfoTemplate({ destinations, cost, dates }) {
+  const title = destinations ? createTitle(destinations) : '...';
+  const tripDates = `${dates ? dayjs(dates.start).format(DateFormats.DAY_HUMAN) : '...'}${dates && dates.end ? `&nbsp;&mdash;&nbsp;${dayjs(dates.end).format(DateFormats.DAY_HUMAN)}` : ''}`;
   return `<section class="trip-main__trip-info  trip-info">
             <div class="trip-info__main">
-              <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+              <h1 class="trip-info__title">
+                ${title}
+              </h1>
 
-              <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+              <p class="trip-info__dates">
+                ${tripDates}
+              </p>
             </div>
 
             <p class="trip-info__cost">
-              Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+              Total: &euro;&nbsp;
+              <span class="trip-info__cost-value">
+                ${cost ?? '0'}
+              </span>
             </p>
           </section>`;
 }
 
 export default class TripInfoView extends AbstractView {
+  #destinations = null;
+  #cost = null;
+  #dates = null;
+
+  constructor({ destinations = null, cost = null, dates = null }) {
+    super();
+    this.#destinations = destinations;
+    this.#cost = cost;
+    this.#dates = dates;
+  }
+
   get template() {
-    return createTripInfoTemplate();
+    return createTripInfoTemplate({
+      destinations: this.#destinations,
+      cost: this.#cost,
+      dates: this.#dates,
+    });
   }
 }
